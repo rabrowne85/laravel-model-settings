2835f57fe1ca280439e6351566b10a31322fdb6a
Richard Browne <r.a.browne@gmail.com>
2020-12-08 13:17:24 +0000

    Add config variable for class name.

--- config/model_settings.php
+++ config/model_settings.php
@@ -3,6 +3,7 @@
 return [
     'settings_field_name' => env('MODEL_SETTINGS_FIELD_NAME', 'settings'),
     'settings_table_name' => env('MODEL_SETTINGS_TABLE_NAME', 'model_settings'),
+    'settings_class_name' => env('MODEL_SETTINGS_CLASS_NAME', Glorand\Model\Settings\Models\ModelSettings::class),
     'settings_persistent' => env('MODEL_SETTINGS_PERSISTENT', true),
     'settings_table_use_cache' => env('MODEL_SETTINGS_TABLE_USE_CACHE', true),
     'settings_table_cache_prefix' => env('MODEL_SETTINGS_TABLE_CACHE_PREFIX', 'model_settings:'),
